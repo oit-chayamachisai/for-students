ce46d1d1a4388cec3ae357e2cb62c7c221c20076
oit-chayamachisai <oit-chayamachisai@gmail.com>
2021-03-19 18:06:51 +0900

    20210319

--- js/style.js
+++ js/style.js
@@ -1,43 +1,42 @@
 $(function () {
-    var topBtn = $('#pagetop');
-    topBtn.hide();
-    //スクロールが300に達したらボタン表示
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            topBtn.fadeIn();
-        } else {
-            topBtn.fadeOut();
-        }
-    });
-    //スクロールでトップへもどる
-    topBtn.click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500);
-        return false;
-    });
+  var topBtn = $('#pagetop');
+  topBtn.hide();
+  //スクロールが300に達したらボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
+    }
+  });
+  //スクロールでトップへもどる
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
 });
 
 // a[href^=#1]
 $(function () {
   $('.js-btn, .anchor').on('click', function () {// js-btnクラスをクリックすると、
     $('.menu , .btn-line').toggleClass('open');// メニューとバーガーの線にopenクラスをつけ外しする
+  })
 
-    })
-
-    $('a[href^=#]').on('click', function (){
-      // スクロールの速度
-      var speed = 400; // ミリ秒
-      // アンカーの値取得
-      var href= $(this).attr("href");
-      // 移動先を取得
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      // 移動先を数値で取得
-      var position = target.offset().top;
-      // スムーススクロール
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-      return false;
-    })
+  $('a[href^=#]').on('click', function (){
+    // スクロールの速度
+    var speed = 400; // ミリ秒
+    // アンカーの値取得
+    var href= $(this).attr("href");
+    // 移動先を取得
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    // 移動先を数値で取得
+    var position = target.offset().top;
+    // スムーススクロール
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  })
 
 });
 // a[href^=#1]
